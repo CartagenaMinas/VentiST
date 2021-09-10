91176691f155ad2602820ae3e9ccd27ed16858ab
CartagenaMinas <79287172+CartagenaMinas@users.noreply.github.com>
2021-09-09 21:07:42 -0500

    dia6

--- calculo.py
+++ calculo.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from traitlets.traitlets import default
 import plotly.express as px
 import matplotlib.pyplot as plt
 from PIL import Image
