e25bb18ec115bee983b5bffd3f6afc586665b963
CartagenaMinas <79287172+CartagenaMinas@users.noreply.github.com>
2021-09-09 20:53:45 -0500

    dia4

---
+++ calculo.py
@@ -0,0 +1,126 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from traitlets.traitlets import default
+import plotly.express as px
+import matplotlib.pyplot as plt
+from PIL import Image
+
+ccfm=35.314699885085965
+    
+def calculo():
+    st.title('Calculo de Requerimiento de Aire en Mina')
+    st.write('## REQUERIMIENTO DE AIRE TOTAL (QTo).- Cuando en la operación se utilice equipos con motor petrolero:')
+    ###########################################################################
+    st.write("**a) Caudal requerido por el número de trabajadores (QTr)**")
+    col1, col2 = st.columns([2, 2])
+    image2 = Image.open('imagenes/2.jpg')
+    col1.image(image2)
+    with col1.expander("Info", expanded=False):
+        st.info("""
+        - Donde:
+        - QTr = Caudal total para “n” trabajadores (m³/min)
+        - F = Caudal mínimo por persona de acuerdo a escala establecida en el artículo 247 del reglamento
+        - N = Número de trabajadores de la guardia más numerosa
+        """)
+    image14 = Image.open('imagenes/14.jpg')
+    col1.image(image14)
+    F = col2.number_input('Caudal mínimo',value=4)
+    N = col2.number_input('Número de trabajadores',value=428)
+    Qtr=F*N
+    cfmqtr=Qtr*ccfm
+    a = np.array([[Qtr, round(cfmqtr)]])
+    df = pd.DataFrame(a, index = ['Qtr'], columns=['m³/min', 'CFM'])
+    col2.dataframe(df.style.format("{:.1f}"))
+    st.write("  ")
+    st.write("  ")
+    ##########################################################################
+    st.write("**b) Caudal requerido por el consumo de madera (QMa)**")
+    col1, col2 = st.columns([2, 2])
+    image3 = Image.open('imagenes/3.jpg')
+    col1.image(image3)
+    with col1.expander("Info", expanded=False):
+        st.info("""
+        - Donde:
+        - QMa = Caudal requerido por toneladas de producción (m3/min)
+        - u = Factor de producción, de acuerdo a escala establecida en el segundo párrafo del literal d) del artículo 252 del reglamento
+        - T = Producción en toneladas métricas húmedas por Guardia.
+        """)
+    image4 = Image.open('imagenes/4.jpg')
+    col1.image(image4)
+    T = col2.number_input('Factor de producción',value=10000)
+    u = col2.number_input('Producción por Guardia',value=0)
+    Qma=T*u
+    cfmqma=Qma*ccfm
+    a=np.append(a, [[Qma, round(cfmqma)]], axis= 0)
+    df1 = pd.DataFrame(a, index = ['Qtr','Qma'], columns=['m³/min', 'CFM'])
+    col2.dataframe(df1[1:].style.format("{:.1f}"))
+    st.write("  ")
+    st.write("  ")
+    #########################
+    st.write("**c) Caudal requerido por temperatura en las labores de trabajo (QTe)**")
+    col1, col2 = st.columns([2, 2])
+    image5 = Image.open('imagenes/5.jpg')
+    col1.image(image5)
+    with col1.expander("Info", expanded=False):
+        st.info("""
+        - Donde:
+        - QTe = Caudal por temperatura (m³/min)
+        - Vm = Velocidad mínima
+        - A = Área de la labor promedio.
+        - N = Número de niveles con temperatura mayor a 23°C, de acuerdo a escala establecida en el tercer párrafo del literal d) del artículo 252 del reglamento.
+        """)
+    image6 = Image.open('imagenes/6.jpg')
+    col1.image(image6)
+    vm = col2.number_input('Velocidad mínima',value=30)
+    A = col2.number_input('Área de la labor promedio',value=21.375)
+    ma_N=col2.number_input('Número de niveles con temperatura mayor a 23°C',value=16)
+    Qte=vm*A*ma_N
+    cfmqte=Qte*ccfm
+    a=np.append(a, [[Qte, round(cfmqte)]], axis= 0)
+    df2 = pd.DataFrame(a, index = ['Qtr','Qma','Qte'], columns=['m³/min', 'CFM'])
+    col2.dataframe(df2[2:].style.format("{:.1f}"))
+    st.write("  ")
+    st.write("  ")
+    #########################
+    st.write("**d) Caudal requerido por equipo con motor Petrolero (QEq)    **")
+    col1, col2 = st.columns([2, 2])
+    image7 = Image.open('imagenes/7.jpg')
+    col1.image(image7)
+    with col1.expander("Info", expanded=False):
+        st.info("""
+        - Donde:
+        - QEq = Volumen de aire necesario para la ventilación (m3/min)
+        - HP = Capacidad efectiva de potencia (HPs)
+        - Dm = Disponibilidad mecánica promedio de los equipos (%)
+        - Fu = Factor de utilización promedio de los equipos (%)
+        """)
+    image15= Image.open('imagenes/15.jpg')
+    col1.image(image15)
+    hp = col2.number_input('Total de HP efectivos',value=303.8)
+    with col2.expander("Advertencia", expanded=True):
+        st.info("""
+        - Se debe de calcular el cuadro que se ejemplifica en el lado izquierdo según la mina que se esté trabajando
+        """)
+    Qeq=3*hp
+    cfmqte=Qeq*ccfm
+    a=np.append(a, [[Qeq, round(cfmqte)]], axis= 0)
+    df3 = pd.DataFrame(a, index = ['Qtr','Qma','Qte','Qeq'], columns=['m³/min', 'CFM'])
+    col2.dataframe(df3[3:].style.format("{:.1f}"))
+    st.write("  ")
+    st.write("  ")
+    #############################################
+    st.write("**e) Caudal requerido por fugas (QFu)**")
+    image9 = Image.open('imagenes/9.jpg')   
+    st.image(image9)
+    image8 = Image.open('imagenes/8.jpg')
+    st.image(image8)
+    image1 = Image.open('imagenes/1.jpg')
+    st.image(image1)
+    dfs=df3.sum().values
+    dfs=pd.DataFrame(dfs,columns=["Qt1"], index =['m³/min', 'CFM'])
+    dfs["Qfu"]=(dfs["Qt1"]*0.15)
+    dfs['Qto']=dfs['Qt1']+dfs["Qfu"]
+    st.dataframe(dfs.style.format("{:.1f}"))
+    #############################
+
---
+++ requerimiento.py
@@ -0,0 +1,168 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+from traitlets.traitlets import default
+import plotly.express as px
+import matplotlib.pyplot as plt
+from PIL import Image
+
+    
+def main():
+    st.title('Requerimiento de Aire en Mina Subterránea D.S. N° 023-2017-EM')
+    col1, col2 = st.columns([3, 1])
+    #col1.write('En minas subterráneas, el principio básico es proveer la suficiente cantidad de aire fresco a los frentes de trabajo y remover los contaminantes generados como el polvo respirable, productos de combustión del diesel, gases de la mina y el exceso de calor. Por lo que realizar el calculo correcto de esta cantidad es fundamental, en esta oportunidad nos vamos a basar en las leyes de Perú. ')
+    col1.markdown("<div style='text-align: justify'>En minas subterráneas, el principio básico es proveer la suficiente cantidad de aire fresco a los frentes de trabajo y remover los contaminantes generados como el polvo respirable, productos de combustión del diesel, gases de la mina y el exceso de calor. Por lo que realizar el calculo correcto de esta cantidad es fundamental, en esta oportunidad nos vamos a basar en las leyes de Perú.</div>", unsafe_allow_html=True)
+    col1.markdown('### CÁLCULO DE REQUERIMIENTO DE AIRE')
+    tipo = ['Operación con equipo de motor petrolero', 'Operación sin equipo de motor petrolero']
+    opcion = col1.selectbox("Tipo de operación subterránea:", tipo)
+    image = Image.open('imagenes/Capture.jpg')
+    col2.image(image, caption='Ventilación en minería subterránea')
+
+    
+    
+
+    if opcion == 'Operación con equipo de motor petrolero':
+        st.write('## REQUERIMIENTO DE AIRE TOTAL (QTo).- Cuando en la operación se utilice equipos con motor petrolero:')
+        with st.expander("REQUERIMIENTO DE AIRE", expanded=True):
+            st.write('La demanda de aire al interior de la mina debe ser calculada de acuerdo al literal d) del artículo 252 del reglamento, considerando la fórmula siguiente:')
+            image1 = Image.open('imagenes/1.jpg')
+            st.image(image1)
+            st.info("""
+            - Donde:
+            - QTo = Caudal total para la operación
+            - QT1 = La sumatoria de caudal requerido por:
+            - a) El número de trabajadores (QTr)
+            - b) El consumo de madera (QMa)
+            - c) Temperatura en labores de trabajo (QTe)
+            - d) Equipos con motor petrolero (QEq)
+            - QFu = 15% del QT1
+            """)
+            st.write("**a) Caudal requerido por el número de trabajadores (QTr)**")
+            image2 = Image.open('imagenes/2.jpg')
+            st.image(image2)
+            st.info("""
+            - Donde:
+            - QTr = Caudal total para “n” trabajadores (m³/min)
+            - F = Caudal mínimo por persona de acuerdo a escala establecida en el artículo 247 del reglamento
+            - N = Número de trabajadores de la guardia más numerosa
+            """)
+            image14 = Image.open('imagenes/14.jpg')
+            st.image(image14)
+            st.write("**b) Caudal requerido por el consumo de madera (QMa)**")
+            image3 = Image.open('imagenes/3.jpg')
+            st.image(image3)
+            st.info("""
+            - Donde:
+            - QMa = Caudal requerido por toneladas de producción (m3/min)
+            - u = Factor de producción, de acuerdo a escala establecida en el segundo párrafo del literal d) del artículo 252 del reglamento
+            - T = Producción en toneladas métricas húmedas por Guardia.
+            """)
+            image4 = Image.open('imagenes/4.jpg')
+            st.image(image4)
+            st.write("**c) Caudal requerido por temperatura en las labores de trabajo (QTe)**")
+            image5 = Image.open('imagenes/5.jpg')
+            st.image(image5)
+            st.info("""
+            - Donde:
+            - QTe = Caudal por temperatura (m³/min)
+            - Vm = Velocidad mínima
+            - A = Área de la labor promedio.
+            - N = Número de niveles con temperatura mayor a 23°C, de acuerdo a escala establecida en el tercer párrafo del literal d) del artículo 252 del reglamento.
+            """)
+            image6 = Image.open('imagenes/6.jpg')
+            st.image(image6)
+
+            st.write("**d) Caudal requerido por equipo con motor Petrolero (QEq)    **")
+            image7 = Image.open('imagenes/7.jpg')
+            st.image(image7)
+            st.info("""
+            - Donde:
+            - QEq = Volumen de aire necesario para la ventilación (m3/min)
+            - HP = Capacidad efectiva de potencia (HPs)
+            - Dm = Disponibilidad mecánica promedio de los equipos (%)
+            - Fu = Factor de utilización promedio de los equipos (%)
+            """)
+
+            st.write("**e) Caudal requerido por fugas (QFu)**")
+            image8 = Image.open('imagenes/8.jpg')
+            st.image(image8)
+            st.write("Donde:")
+            image9 = Image.open('imagenes/9.jpg')
+            st.image(image9)
+
+
+
+    elif opcion == 'Operación sin equipo de motor petrolero':
+        st.write('## REQUERIMIENTO DE AIRE TOTAL (QTo).- Cuando en la operación no se utilicen equipos con motor petrolero')
+        with st.expander("REQUERIMIENTO DE AIRE", expanded=True):
+            st.write('Debe calcularse el caudal total para la operación conforme la fórmula que se detalla a continuación y luego **compararla con el caudal por el consumo de explosivos. Luego de obtener cada uno de los valores se determina como Requerimiento de Aire Total el de mayor valor**. La demanda de aire al interior de la mina debe ser calculada de acuerdo al literal d) del artículo 252 del reglamento, considerando la fórmula siguiente:')
+            image11 = Image.open('imagenes/11.jpg')
+            st.image(image11)
+            st.info("""
+            - Donde:
+            - QTo = Caudal total para la operación
+            - QT1 = La sumatoria de caudal requerido por:
+            - a) el número de trabajadores (QTr)
+            - b) el consumo de madera (QMa)
+            - c) temperatura en labores de trabajo (QTe)
+            - QFu = 15% del QT1
+            """)
+            st.write("**a) Caudal requerido por el número de trabajadores (QTr)**")
+            image2 = Image.open('imagenes/2.jpg')
+            st.image(image2)
+            st.info("""
+            - Donde:
+            - QTr = Caudal total para “n” trabajadores (m³/min)
+            - F = Caudal mínimo por persona de acuerdo a escala establecida en el artículo 247 del reglamento
+            - N = Número de trabajadores de la guardia más numerosa
+            """)
+            image14 = Image.open('imagenes/14.jpg')
+            st.image(image14)
+            st.write("**b) Caudal requerido por el consumo de madera (QMa)**")
+            image3 = Image.open('imagenes/3.jpg')
+            st.image(image3)
+            st.info("""
+            - Donde:
+            - QMa = Caudal requerido por toneladas de producción (m3/min)
+            - u = Factor de producción, de acuerdo a escala establecida en el segundo párrafo del literal d) del artículo 252 del reglamento
+            - T = Producción en toneladas métricas húmedas por Guardia.
+            """)
+            image4 = Image.open('imagenes/4.jpg')
+            st.image(image4)
+            st.write("**c) Caudal requerido por temperatura en las labores de trabajo (QTe)**")
+            image5 = Image.open('imagenes/5.jpg')
+            st.image(image5)
+            st.info("""
+            - Donde:
+            - QTe = Caudal por temperatura (m³/min)
+            - Vm = Velocidad mínima
+            - A = Área de la labor promedio.
+            - N = Número de niveles con temperatura mayor a 23°C, de acuerdo a escala establecida en el tercer párrafo del literal d) del artículo 252 del reglamento.
+            """)
+            image6 = Image.open('imagenes/6.jpg')
+            st.image(image6)
+
+
+            st.write("**d) Caudal requerido por fugas (QFu)**")
+            image8 = Image.open('imagenes/8.jpg')
+            st.image(image8)
+            st.write("Donde:")
+            image12 = Image.open('imagenes/12.jpg')
+            st.image(image12)
+
+            st.write("**e) Caudal requerido por consumo de explosivo (QEx)**")
+            st.write("Cuando en la operación no se utilicen equipos con motor petrolero, debe calcularse y tenerse en cuenta la necesidad de aire requerido por consumo de explosivos, conforme lo siguiente:")
+            image13 = Image.open('imagenes/13.jpg')
+            st.image(image13)
+            st.info("""
+            - Donde:
+            - Qex = Caudal de aire requerido por consumo de explosivo detonado (m3/min)
+            - A = Área promedio de labores (m2)
+            - V = Velocidad mínima requerida según norma (m/min)
+            - N = Número de niveles en voladura
+            """)
+
+
+
+
+
